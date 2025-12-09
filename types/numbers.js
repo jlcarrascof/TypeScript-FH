56cfacf44c01dc4d83e454eae456be086a82e354
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-09 10:50:32 -0400

    Add conditional logic to numbers.js and numbers.ts to evaluate avengers against villians

--- types/numbers.js
+++ types/numbers.js
@@ -2,4 +2,11 @@
 (() => {
     let avengers;
     console.log(avengers);
+    const villians = 20;
+    if (avengers < villians) {
+        console.log('We are in problems');
+    }
+    else {
+        console.log('We are safe');
+    }
 })();
--- types/numbers.ts
+++ types/numbers.ts
@@ -4,4 +4,12 @@
 
     console.log(avengers);
 
+    const villians = 20;
+
+    if (avengers < villians) {
+        console.log('We are in problems');
+    } else {
+        console.log('We are safe');
+    }      
+
 })();
