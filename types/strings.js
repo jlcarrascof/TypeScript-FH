93d44de96b8a8e174f70bdd9504f508172a00c54
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-14 14:00:38 -0400

    Add string constants and logging to strings.js and strings.ts

--- types/strings.js
+++ types/strings.js
@@ -1,3 +1,7 @@
 "use strict";
 (() => {
+    const batman = 'Batman';
+    const greenLantern = "Green Lantern";
+    const blackVulcano = `Black Vulcano`;
+    console.log(batman.toUpperCase());
 })();
--- types/strings.ts
+++ types/strings.ts
@@ -1,6 +1,10 @@
 (() => {
 
+    const batman: string = 'Batman';
+    const greenLantern: string = "Green Lantern";
+    const blackVulcano: string = `Black Vulcano`;
 
+    console.log( batman.toUpperCase());
 
 
 })()
