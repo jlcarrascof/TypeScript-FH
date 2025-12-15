d872c6898660bf77430948d43e40176b2af6eb7a
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-15 12:59:34 -0400

    Add logging for exists and power variables in any.js and any.ts

--- types/any.js
+++ types/any.js
@@ -6,4 +6,7 @@
     avenger = 'Dr Strange';
     console.log(avenger.charAt(0));
     avenger = 150.23256415;
+    console.log(avenger.toFixed(2));
+    console.log(exists);
+    console.log(power);
 })();
--- types/any.ts
+++ types/any.ts
@@ -8,6 +8,9 @@
     console.log( avenger.charAt(0) );
 
     avenger = 150.23256415;
+    console.log( avenger.toFixed(2) );
 
+    console.log( exists );
+    console.log( power );
     
 })()
