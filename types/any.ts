239d88c005882b482045b6149af1703bd193f644
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-15 13:46:10 -0400

    Refactor character access to use type assertion in any.ts and comment out original log in any.js

--- types/any.ts
+++ types/any.ts
@@ -5,7 +5,8 @@
     let power;
     
     avenger = 'Dr Strange';
-    console.log( avenger.charAt(0) );
+    // console.log( avenger.charAt(0) );
+    console.log( (avenger as string).charAt(0) );
 
     avenger = 150.23256415;
     console.log( avenger.toFixed(2) );
