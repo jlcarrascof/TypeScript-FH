5ece551d641ef29b0745291aa1da4a8b3b2e8676
Javier Martinez <entrenamientospuntonet@gmail.com>
2025-12-15 12:48:21 -0400

    Add initial implementation of avenger example in any.js and any.ts

---
+++ types/any.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+    let avenger = 123;
+    let exists;
+    let power;
+    avenger = 'Dr Strange';
+    console.log(avenger.charAt(0));
+    avenger = 150.23256415;
+})();
---
+++ types/any.ts
@@ -0,0 +1,13 @@
+(() => {
+
+    let avenger: any = 123;
+    let exists;
+    let power;
+    
+    avenger = 'Dr Strange';
+    console.log( avenger.charAt(0) );
+
+    avenger = 150.23256415;
+
+    
+})()
